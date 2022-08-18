6839dbab0df4802921e60afd373916f83e3e8d83
Roberto Telles <robertotelles00@gmail.com>
2022-08-18 11:33:58 -0300

    Upload 18/08/2022 2

--- EntitiesServices/Model/UNIDADE.cs
+++ EntitiesServices/Model/UNIDADE.cs
@@ -23,6 +23,7 @@ namespace EntitiesServices.Model
         public int UNID_CD_ID { get; set; }
         public string UNID_NM_NOME { get; set; }
         public int UNID_IN_ATIVO { get; set; }
+        public string UNID_SG_SIGLA { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<PRODUTO> PRODUTO { get; set; }
